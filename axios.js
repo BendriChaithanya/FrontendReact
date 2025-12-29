64f54a3cc70f4955542e2daff58a07d81a7fa392
BendriChaithanya <chaithanyabendri@gmail.com>
2025-12-29 11:37:37 +0530

    second commit

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:9065/api/v1/products",
+  baseURL: "https://github.com/BendriChaithanya/BackendExpress",
 });
 
 api.interceptors.request.use((config) => {

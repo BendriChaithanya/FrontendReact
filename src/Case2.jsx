dbddabe016193d24bbaa554d28276dc5bd1be5f3
BendriChaithanya <chaithanyabendri@gmail.com>
2026-01-03 19:43:24 +0530

    Fix case-sensitive routes

---
+++ src/Case2.jsx
@@ -0,0 +1,10 @@
+function Case2({numb1,numb2})
+{
+    return(
+        <>
+        <h2>Number1:{numb1}</h2>
+        <h2>Number2:{numb2}</h2>
+        <h3>sum:{numb1+numb2}</h3>
+        </>
+    )
+}export default Case2;
